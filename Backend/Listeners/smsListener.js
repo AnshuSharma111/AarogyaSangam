f1b39a26693194775f6d500ef14202ba9226953e
Amitrajeet <142527664+amitrajeet7635@users.noreply.github.com>
2025-02-15 21:04:46 +0530

    Delete Backend directory

--- Backend/Listeners/smsListener.js
+++
@@ -1,9 +0,0 @@
-const smsEvents = require("../eventBus"); // Import shared event emitter
-const appointmentController = require("../Services/appointment"); // Import appointment controller
-const smsController = require("../Services/sms"); // Import sms controller
-
-smsEvents.on("register", appointmentController.register); // Listen for register event and call register function
-smsEvents.on("book", appointmentController.book); // Listen for book event and call book function
-smsEvents.on("confirm", appointmentController.confirm); // Listen for confirm event and call confirm function
-smsEvents.on("sendSMS", smsController.send); // Listen for appointmentBooked event and call confirm function
-smsEvents.on("error", appointmentController.errorHandler); // Listen for error event and call errorHandler function
--- Backend/eventBus.js
+++
@@ -1,4 +0,0 @@
-const EventEmitter = require("events");
-const smsEvents = new EventEmitter(); // Create a universal event emitter
-
-module.exports = smsEvents; // Export shared event emitter
